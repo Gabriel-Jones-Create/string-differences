fade6cd712ee1195596e672065dccd90700e57f9
Gabriel Jones <gjones2023@chadwickschool.org>
2021-09-21 09:00:02 -0700

    style: Clean up code formatting
    I added comments and fixed the formatting for the project

--- StringDifferences/src/StringDifferences.java
+++ StringDifferences/src/StringDifferences.java
@@ -1,30 +1,35 @@
 import java.util.Scanner;
+
 public class StringDifferences {
 	public static void main(String[] args) {
-		Scanner prompt1 = new Scanner(System.in);
-		System.out.println("Enter string one:");
-		Scanner prompt2 = new Scanner(System.in);
-		System.out.println("Enter string two:");
-		String inputStr1 = prompt1.nextLine();
-		String inputStr2 = prompt2.nextLine();
-		int shortestWordLength = 0;
-		int diffCounter = 0;
-		
-		if(inputStr1.length() > inputStr2.length() ) {
+		Scanner scan = new Scanner(System.in); // creates a scanner for user input
+		System.out.println("Enter string one:");// gives prompt for String one
+		String inputStr1 = scan.nextLine();// sets String one to the input from the scanner
+		System.out.println("Enter string two:");// gives prompt for String two
+		String inputStr2 = scan.nextLine();// sets String two to the input from the scanner
+		int shortestWordLength = 0; // creates shortest word length variable
+		int diffCounter = 0; // sets a difference counter variable
+
+		if (inputStr1.length() > inputStr2.length()) {// tests to see which string is shorter to use for iteration
+														// number in the for loop
 			shortestWordLength = inputStr2.length();
-		}
-		else {
+		} else {
 			shortestWordLength = inputStr1.length();
 		}
-		for( int i = 0 ; i < shortestWordLength; i++) {
-			if(!inputStr1.substring(i,i+1).equals(inputStr2.substring(i,i+1))) {
-				System.out.println(i + " " + inputStr1.substring(i,i+1) + " " + inputStr2.substring(i,i+1));
+		for (int i = 0; i < shortestWordLength; i++) {
+			if (!inputStr1.substring(i, i + 1).equals(inputStr2.substring(i, i + 1))) {
+				/*
+				 * iterates for the amount of the shortest string if the letters are different
+				 * in the string, it prints the index and the difference. It also adds a counter
+				 * of the number of differences.
+				 */
+				System.out.println(i + " " + inputStr1.substring(i, i + 1) + " " + inputStr2.substring(i, i + 1));
 				diffCounter++;
-				
+
 			}
-			
+
 		}
-		System.out.println("Number of Differences: "+ diffCounter);
-		
+		System.out.println("Number of Differences: " + diffCounter); // prints the number of differences
+
 	}
 }
