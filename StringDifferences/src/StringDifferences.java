ba805a361efb961d1e6fe5fcd94451f6dfdec64d
Gabriel Jones <gjones2023@chadwickschool.org>
2021-09-30 09:45:45 -0700

    style: Fix comment formatting
    Fixed comment formatting according to Mr. DB's comments

--- StringDifferences/src/StringDifferences.java
+++ StringDifferences/src/StringDifferences.java
@@ -10,26 +10,30 @@ public class StringDifferences {
 		int shortestWordLength = 0; // creates shortest word length variable
 		int diffCounter = 0; // sets a difference counter variable
 
-		if (inputStr1.length() > inputStr2.length()) {// tests to see which string is shorter to use for iteration
-														// number in the for loop
+		/*
+		 * tests to see which string is shorter to use for iteration number in the for
+		 * loop
+		 */
+		if (inputStr1.length() > inputStr2.length()) {
 			shortestWordLength = inputStr2.length();
 		} else {
 			shortestWordLength = inputStr1.length();
 		}
+
+		/*
+		 * iterates for the amount of the shortest string if the letters are different
+		 * in the string, it prints the index and the difference. It also adds a counter
+		 * of the number of differences.
+		 */
 		for (int i = 0; i < shortestWordLength; i++) {
 			if (!inputStr1.substring(i, i + 1).equals(inputStr2.substring(i, i + 1))) {
-				/*
-				 * iterates for the amount of the shortest string if the letters are different
-				 * in the string, it prints the index and the difference. It also adds a counter
-				 * of the number of differences.
-				 */
 				System.out.println(i + " " + inputStr1.substring(i, i + 1) + " " + inputStr2.substring(i, i + 1));
 				diffCounter++;
 
 			}
-
 		}
-		System.out.println("Number of Differences: " + diffCounter); // prints the number of differences
 
+		// prints the number of differences
+		System.out.println("Number of Differences: " + diffCounter);
 	}
 }
